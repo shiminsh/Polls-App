a217f57586db83e80042176a6b3b8b469f00b8bf
Shalini Roy <shaliniroy012@gmail.com>
2014-05-02 23:21:38 +0530

    register the models for admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from polls.models import Poll
+from polls.models import Choice
 
-# Register your models here.
+admin.site.register(Poll)
+admin.site.register(Choice)
+
+
